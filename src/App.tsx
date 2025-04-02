063942229d1b97224cbc59cd4fe96bddec139cc4
CassandraDM <cassandra.delmas-marchiset@mail-esd.com>
2025-04-02 11:58:39 +0200

    hahahahahhahahha

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useTodoListContext } from "./context/TodoListContext/hook";
 const App = () => {
   const { todoList } = useTodoListContext();
   return (
-    <section className="container w-[100vw] m-auto mt-4 flex items-center justify-center">
+    <section className="container w-[100vw] bg-green m-auto mt-4 flex items-center justify-center">
       <TodoList list={todoList} />
     </section>
   );
